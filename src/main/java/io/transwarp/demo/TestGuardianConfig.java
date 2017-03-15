a2e28e1e62d10476be80e0ee83de26e8421b9e37
Xiaming Chen <chenxm35@gmail.com>
2017-03-15 10:03:58 +0800

    Update guardian quota

---
+++ src/main/java/io/transwarp/demo/TestGuardianConfig.java
@@ -0,0 +1,36 @@
+package io.transwarp.demo;
+
+import io.transwarp.guardian.common.exception.GuardianClientException;
+import io.transwarp.guardian.plugins.inceptor.GuardianSchedulerConfProvider;
+import io.transwarp.inceptor.scheduler.AccessType;
+import io.transwarp.inceptor.scheduler.PrivilegedEntity;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.hive.conf.HiveConf;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class TestGuardianConfig {
+
+    public static void main(String[] args) throws IOException, GuardianClientException {
+        HiveConf hconf = new HiveConf();
+        hconf.set("hive.service.id", "inceptor1");
+        GuardianSchedulerConfProvider conf = new GuardianSchedulerConfProvider(hconf);
+
+        // FAIR configuration
+        InputStream is = conf.getConfigurationInputStream("fair");
+        System.out.println(IOUtils.toString(is));
+
+        // Furion configuration
+        is = conf.getConfigurationInputStream("furion");
+        System.out.println(IOUtils.toString(is));
+
+        // Check permission
+        // chenxm, root
+        PrivilegedEntity pe = new PrivilegedEntity(PrivilegedEntity.EntityType.QUEUE, "root");
+        conf.checkPermission(AccessType.SUBMIT_APP, pe, "chenxm");
+
+
+    }
+}
--- src/main/java/io/transwarp/demo/TestGuardianQuota.java
+++ src/main/java/io/transwarp/demo/TestGuardianQuota.java
@@ -7,23 +7,23 @@ import io.transwarp.guardian.client.GuardianClientFactory;
 import io.transwarp.guardian.common.exception.GuardianClientException;
 import io.transwarp.guardian.common.model.QuotaVo;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TestGuardianQuota {
-    private static String QUOTA_COMPONENT = "INCEPTOR_SCHEDULER";
-    private static String QUOTA_USER_POOLS = "is.user.permitted.pools";
-    private static String QUOTA_USER_QUERIES_POOL = "is.max.active.queries.per.pool";
-    private static String QUOTA_USER_SESSIONS = "is.max.active.sessions.per.user";
-    private static String QUOTA_USER_QUERIES_SESSION = "is.max.active.queries.per.session";
+    private static String QUOTA_COMPONENT = "inceptor1";
+    private static String QUOTA_USER_POOLS = "is.quota.user.permitted.pools";
+    private static String QUOTA_USER_QUERIES_POOL = "is.quota.user.pool";
+    private static String QUOTA_USER_SESSIONS = "is.quota.user.session";
+    private static String QUOTA_USER_TOTAL = "is.quota.user.total";
 
     public static void main(String[] args) throws GuardianClientException {
         GuardianClient client = GuardianClientFactory.getInstance();
         client.login();
         GuardianAdmin admin = GuardianAdminFactory.getInstance();
 
-        // create new quota
-        QuotaVo quota = new QuotaVo(QUOTA_COMPONENT, "chenxm");
+        // create new quota for chen
+        List<String> dataSource = Arrays.asList("FURION_SCHEDULER_USER", "chenxm");
+        QuotaVo quota = new QuotaVo(QUOTA_COMPONENT, dataSource);
         try {
             admin.deleteQuota(quota);
         } catch (Exception e) {
@@ -31,44 +31,30 @@ public class TestGuardianQuota {
         }
         Map<String, Object> props = new HashMap<String, Object>();
         props.put(QUOTA_USER_POOLS, "H0:1,M0:1,L0:1");
-        props.put(QUOTA_USER_QUERIES_POOL, "1");
-        props.put(QUOTA_USER_SESSIONS, "2");
-        props.put(QUOTA_USER_QUERIES_SESSION, "1");
+        props.put(QUOTA_USER_QUERIES_POOL, "10");
+        props.put(QUOTA_USER_SESSIONS, "10");
+        props.put(QUOTA_USER_TOTAL, "1");
         quota.setProperties(props);
         admin.addQuota(quota);
 
-        System.out.println(client.readQuota(new QuotaVo(QUOTA_COMPONENT, "chenxm")).toString());
+        System.out.println(client.readQuota(new QuotaVo(QUOTA_COMPONENT, dataSource)).toString());
+        List<QuotaVo> allQuotas = client.listQuotas(QUOTA_COMPONENT, Arrays.asList("FURION_SCHEDULER_USER"));
 
-        quota = new QuotaVo(QUOTA_COMPONENT, "king");
+        dataSource = Arrays.asList("FURION_SCHEDULER_USER", "king");
+        quota = new QuotaVo(QUOTA_COMPONENT, dataSource);
         try {
             admin.deleteQuota(quota);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
         props.clear();
-        props.put(QUOTA_USER_POOLS, "**");
+        props.put(QUOTA_USER_POOLS, "*");
         props.put(QUOTA_USER_QUERIES_POOL, "5");
         props.put(QUOTA_USER_SESSIONS, "2");
-        props.put(QUOTA_USER_QUERIES_SESSION, "3");
+        props.put(QUOTA_USER_TOTAL, "3");
         quota.setProperties(props);
         admin.addQuota(quota);
 
-        System.out.println(client.readQuota(new QuotaVo(QUOTA_COMPONENT, "king")).toString());
-
-        quota = new QuotaVo(QUOTA_COMPONENT, "anonymous");
-        try {
-            admin.deleteQuota(quota);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        props.clear();
-        props.put(QUOTA_USER_POOLS, "H0,M0");
-        props.put(QUOTA_USER_QUERIES_POOL, "1");
-        props.put(QUOTA_USER_SESSIONS, "2");
-        props.put(QUOTA_USER_QUERIES_SESSION, "1");
-        quota.setProperties(props);
-        admin.addQuota(quota);
-
-        System.out.println(client.readQuota(new QuotaVo(QUOTA_COMPONENT, "anonymous")).toString());
+        System.out.println(client.readQuota(new QuotaVo(QUOTA_COMPONENT, dataSource)).toString());
     }
 }
